335f8ba2a91441edf85668372005a2eef8e5406b
fatualux <35587292+fatualux@users.noreply.github.com>
2024-05-29 11:21:46 +0200

    convert watt to kw

--- modules/overview.py
+++ modules/overview.py
@@ -32,6 +32,20 @@ class Overview:
         return re.sub(
             f"([{re.escape(escape_chars)}])", r'\\\1', text)
 
+    @staticmethod
+    def convert_to_kwh(value):
+        """
+        Helper function to convert Wh to kWh.
+        """
+        return value / 1000.0
+
+    @staticmethod
+    def convert_to_kw(value):
+        """
+        Helper function to convert W to kW.
+        """
+        return value / 1000.0
+
     def print_site_overview(self, overview_data):
         overview = overview_data.get('overview')
         if overview:
@@ -39,13 +53,13 @@ class Overview:
             overview_str += (
                 f"Ultimo aggiornamento: {self.escape_markdown(overview.get('lastUpdateTime'))}\n"
                 "Dati complessivi:\n"
-                f"  Energia: {self.escape_markdown(str(overview['lifeTimeData']['energy']))}\n"
+                f"  Energia prodotta: {self.escape_markdown(f'{self.convert_to_kwh(overview['lifeTimeData']['energy']):.3f}')} kWh\n"
                 "Dati ultimo anno:\n"
             )
             last_year_data = overview.get('lastYearData')
             if last_year_data:
                 overview_str += (
-                    f"  Energia: {self.escape_markdown(str(last_year_data.get('energy')))}\n"
+                    f"  Energia prodotta: {self.escape_markdown(f'{self.convert_to_kwh(last_year_data.get('energy')):.3f}')} kWh\n"
                 )
             else:
                 overview_str += "  No data available for last year.\n"
@@ -53,7 +67,7 @@ class Overview:
             last_month_data = overview.get('lastMonthData')
             if last_month_data:
                 overview_str += (
-                    f"  Energia: {self.escape_markdown(str(last_month_data.get('energy')))}\n"
+                    f"  Energia prodotta: {self.escape_markdown(f'{self.convert_to_kwh(last_month_data.get('energy')):.3f}')} kWh\n"
                 )
             else:
                 overview_str += "  No data available for last month.\n"
@@ -61,13 +75,13 @@ class Overview:
             last_day_data = overview.get('lastDayData')
             if last_day_data:
                 overview_str += (
-                    f"  Energia: {self.escape_markdown(str(last_day_data.get('energy')))}\n"
+                    f"  Energia prodotta: {self.escape_markdown(f'{self.convert_to_kwh(last_day_data.get('energy')):.3f}')} kWh\n"
                 )
             else:
                 overview_str += "  No data available for last day.\n"
             overview_str += "Potenza attuale:\n"
             overview_str += (
-                f"  Potenza: {self.escape_markdown(str(overview['currentPower']['power']))}\n"
+                f"  Potenza: {self.escape_markdown(f'{self.convert_to_kw(overview['currentPower']['power']):.3f}')} kW\n"
             )
             return overview_str
         else:
